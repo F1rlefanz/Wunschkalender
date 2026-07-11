343c88fda2e116071dd1ff968b03a434a9b97adf
F1rlefanz <158139941+F1rlefanz@users.noreply.github.com>
2026-07-11 09:44:18 +0200

    feat: make server port configurable via environment variable

--- server.ts
+++ server.ts
@@ -42,7 +42,7 @@ function saveDb() {
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
   const httpServer = http.createServer(app);
   const io = new SocketIOServer(httpServer, {
     cors: { origin: '*' }
